562a5aa2e059c8c0b1c1f8c788775970d0a1a86f
Marco Ippel <marco@ippelsoft.nl>
2021-11-03 18:28:52 +0100

    Update

--- src/GoBuildQueueMonitor/run.sh
+++ src/GoBuildQueueMonitor/run.sh
@@ -1,4 +1,4 @@
-go build .
+go build -o buildqueuemonitor
 docker rm -f buildqueuemonitor
-docker build -t buildqueuemonitor .
+docker build -t buildqueuemonitor ../.
 docker run -d --name buildqueuemonitor -p 85:8080  buildqueuemonitor
